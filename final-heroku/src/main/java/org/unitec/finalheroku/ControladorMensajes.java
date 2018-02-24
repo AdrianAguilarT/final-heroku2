de474273c88284fb9e5b3d66dfdf5bcd681541ce
AdrianAguilarT <31869875+AdrianAguilarT@users.noreply.github.com>
2018-02-23 21:06:01 -0600

    Add files via upload
    23/02/18

---
+++ final-heroku/src/main/java/org/unitec/finalheroku/ControladorMensajes.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.finalheroku;
+
+
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class ControladorMensajes {
+    
+    @Autowired RepositorioMensajito repoMensa;
+    
+    //Buscar Todos
+    @CrossOrigin
+   @RequestMapping(value="/mensajito",method=RequestMethod.GET,
+           headers={"Accept=application/json"})
+   public ArrayList<Mensajito> hola(){
+    
+       return (ArrayList<Mensajito>) repoMensa.findAll();
+   }
+   
+   
+   //Buscar por ID
+    @CrossOrigin
+   @RequestMapping(value="/mensajito",method=RequestMethod.GET,
+           headers={"Accept=application/json"})
+   public Mensajito hola(@PathVariable String id){
+    
+       return repoMensa.findOne(id);
+   }
+   
+   //Actualizar
+   @CrossOrigin
+   @RequestMapping(value="/mensajito/{id}/{titulo}/{cuerpo}", method=RequestMethod.PUT,
+           headers ={"Accept=application/json"})
+   public Estatus actualizar(@PathVariable String id,@PathVariable String titulo, @PathVariable String cuerpo){
+          repoMensa.save(new Mensajito(id,titulo, cuerpo));
+          return new Estatus(true, "Actualizado con éxito");
+       
+   }
+   
+   
+   //Guardar
+   @CrossOrigin
+   @RequestMapping(value="/mensajito/{titulo}/{cuerpo}", method=RequestMethod.POST,
+           headers ={"Accept=application/json"})
+   public Estatus guardar(@PathVariable String titulo, @PathVariable String cuerpo){
+          repoMensa.save(new Mensajito(titulo, cuerpo));
+          return new Estatus(true, "Actualizado con éxito");
+       
+   }
+   
+   //Borrar
+   @CrossOrigin
+   @RequestMapping(value="/mensajito/{id}", method = RequestMethod.DELETE,
+           headers = {"Accept=application/json"})
+           public Estatus borrarMensaje(@PathVariable String id){
+               
+               Estatus estatus=new Estatus(true, "Borrado con exito");
+               repoMensa.delete(new Mensajito(id));
+               
+               return estatus;
+           }
+    
+}
+
+
+
+
+
+
---
+++ final-heroku/src/main/java/org/unitec/finalheroku/Mensajito.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.finalheroku;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.springframework.data.annotation.Id;
+
+/**
+ *
+ * @author T-107
+ */
+public class Mensajito {
+    
+    @Id
+    private String id;
+    
+    private LocalDate fecha;
+    private LocalTime hora;
+
+    public Mensajito(String id) {
+        this.id = id;
+    }
+    
+    private String titulo;
+    private String cuerpo;
+
+    public Mensajito(String id, String titulo, String cuerpo) {
+        this.id = id;
+        this.titulo = titulo;
+        this.cuerpo = cuerpo;
+        fecha=LocalDate.now();
+        hora=LocalTime.now();
+    }
+
+    public Mensajito(String titulo, String cuerpo) {
+        this.titulo = titulo;
+        this.cuerpo = cuerpo;
+        fecha=LocalDate.now();
+          hora=LocalTime.now();
+    }
+
+    public Mensajito() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+    
+    
+}
